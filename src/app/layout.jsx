5562b377c292714919727ff91a102ab35f5deb59
Calvin Roberts <you@example.com>
2023-11-08 14:40:37 -0700

    added shop page and did more clean up of files

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,12 +5,12 @@ import '@/styles/tailwind.css'
 
 export const metadata = {
   title: {
-    template: '%s - Spencer Sharp',
+    template: '%s - papercuts collage',
     default:
-      'Spencer Sharp - Software designer, founder, and amateur astronaut',
+      'papercuts collage - Artist, Musician, Activist',
   },
   description:
-    'I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.',
+    "I'm Emma Roberts. My artist handle is papercuts! I am a local artist living in Salt Lake City, Utah. My passions are art, music, and activism.",
   alternates: {
     types: {
       'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
