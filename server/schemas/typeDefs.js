86bb514f42c38ddbad62c8f45efc7079f11e848d
Calvin Matson-Roberts <calvinmatsonroberts@outlook.com>
2024-02-23 17:11:22 -0500

    server side updates
    added typeDefs, art model, and config connections

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,93 @@
+const typeDefs = `
+  type Category{
+    _id: ID
+    name: String
+  }
+  
+  type Product {
+    _id: ID
+    name: String
+    description: String
+    image: String
+    quantity: Int
+    price: Float
+    category: Category
+  }
+
+  type Order {
+    _id: ID
+    purchaseDate: String
+    products: [Product]
+  }
+
+  type User {
+    _id: ID
+    userName: String
+    email: String
+    bio: String
+    orders: [Order]
+  }
+
+  type Checkout {
+    session: ID
+  }
+
+  type UserWithImages {
+    _id: ID
+    userName: String
+    email: String
+    bio: String
+    images: [ImageDetails!]
+  }
+  
+  type Comment {
+    _id: ID
+    user: User
+    comment: String
+    createdAt: String
+  }
+
+  type ImageDetails{
+    _id: ID
+    image: String
+    title: String!
+    description: String!
+    declaration: Boolean!
+    critique: Boolean
+    price: Float
+    canPurchase: Boolean
+    purchasePrice: Float
+    canDownload: Boolean
+    selectedMedium: String
+    user: User
+    comments: [Comment]
+  }
+
+  type Auth {
+    token: ID
+    user: User
+  }
+  
+  type Query {
+    user: User
+    users: [User]
+    images: [ImageDetails]
+    image(imageId: ID!): ImageDetails
+    usersWithImages: [UserWithImages]
+    categories: [Category]
+    products(category: ID, name: String): [Product]
+    product(_id: ID!): Product
+    order(_id: ID!): Order
+    checkout(products: [ID]!): Checkout
+  }
+
+  type Mutation {
+    addUser(userName: String!, bio: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    uploadImage(userId: ID!, image: String, title: String!, description: String!, declaration: Boolean!, critique: Boolean, price: Float, canDownload: Boolean, purchasePrice: Float, canPurchase: Boolean, selectedMedium: String): ImageDetails!
+    addComment(imageId: ID!, comment: String!): ImageDetails
+    addOrder(products: [ID]!): Order
+    updateProduct(_id: ID!, quantity: Int!): Product
+  }
+  `;
+module.exports = typeDefs;
